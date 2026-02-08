858ce95f755781963626bfd38585c719b009d319
bezael <bezael@gmail.com>
2026-02-08 13:20:45 +0100

    refactor(metrics): remove delay from fetchMetrics method
    - Eliminated the artificial delay in the fetchMetrics method for immediate data retrieval.
    - Cleaned up imports by removing unused delay operator from RxJS.

--- src/app/services/metrics.service.ts
+++ src/app/services/metrics.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { delay, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ServerMetrics } from '../models/server-metrics';
 
 @Injectable({ providedIn: 'root' })
@@ -9,6 +9,6 @@ export class MetricsService {
   private readonly _apiUrl = 'http://localhost:3001/api/metrics';
 
   fetchMetrics(): Observable<ServerMetrics> {
-  return this._http.get<ServerMetrics>(this._apiUrl).pipe(delay(7000));
+  return this._http.get<ServerMetrics>(this._apiUrl);
   }
 }
